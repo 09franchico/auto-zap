5abada94e2bd54b7360e931882bbf9c643edc634
TiagoSPimetel <tiago.pimentel111@gmail.com>
2025-01-25 23:57:37 -0400

    OK

---
+++ IA-deepSeek.py
@@ -0,0 +1,14 @@
+from openai import OpenAI
+
+client = OpenAI(api_key="sk-08a2733fe2b64e8993cedb805f8ce78e", base_url="https://api.deepseek.com")
+
+response = client.chat.completions.create(
+    model="deepseek-chat",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant"},
+        {"role": "user", "content": "Me der um exmeplo de codigo em python ?"},
+    ],
+    stream=False
+)
+
+print(response.choices[0].message.content)
